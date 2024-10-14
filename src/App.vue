425d2a2e630d165bc29bb596adcc296aea4d0b1b
fatalistix <slavabal11@outlook.com>
2024-10-14 21:32:10 +0700

    added missing notifications, changed router-view codestyle

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { Notifications } from '@kyvg/vue3-notification';
 import { RouterView } from 'vue-router';
 
 </script>
 
 <template>
-  <router-view />
+  <RouterView />
+  <Notifications position="bottom center" width="100%"/>
 </template>
 
 <style lang="scss" scoped>
