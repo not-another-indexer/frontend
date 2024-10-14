4508a551d989ee5e211e2d26987b745ff4b2eda2
fatalistix <slavabal11@outlook.com>
2024-10-14 21:32:48 +0700

    removed unnecessary tabs, added register handler

--- src/router.ts
+++ src/router.ts
@@ -1,16 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "./components/login/Login.vue";
+import Register from "./components/register/Register.vue";
 
 const routes = [
-        {
-            path: '/',
-            redirect: '/login',
-        },
-        {
-            path: '/login',
-            component: Login,
-        }
-    ]
+    {
+        path: '/',
+        redirect: '/login',
+    },
+    {
+        path: '/login',
+        component: Login,
+    },
+    {
+        path: '/register',
+        component: Register,
+    },
+]
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
