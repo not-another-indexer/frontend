61eada207cef810aa829cc36dd0691996d2493c8
fatalistix <slavabal11@outlook.com>
2024-11-16 16:16:15 +0700

    added addImage mutation

---
+++ src/mutations/addImage.ts
@@ -0,0 +1,80 @@
+import { useNotification } from "@kyvg/vue3-notification";
+import { AddImageRequest, AddImageResponse } from "protos/gen/nai";
+import { useUserStore } from "../stores/UserStore";
+import { galleryServiceClient } from "../transport";
+import { handleRpcError } from "../utils/error_handler";
+import { defineMutation, useMutation, useQueryCache } from "@pinia/colada";
+import { ref } from "vue";
+
+const notification = useNotification()
+
+const addImage = async (
+  imageBytes: Uint8Array, 
+  galleryId: string, 
+  description: string,
+  extension: string,
+): Promise<AddImageResponse> => {
+  const userStore = useUserStore()
+
+  const promise = userStore.withActualToken<AddImageRequest, AddImageResponse>(token => {
+    const opts = {
+      meta: {
+        "Authorization": "Bearer " + token,
+      }
+    }
+
+    const request = {
+      pChunkData: imageBytes,
+      pMetadata: {
+        pGalleryUuid: galleryId,
+        pDescription: description,
+        pExtension: extension,
+      }
+    }
+
+    return galleryServiceClient.addImage(request, opts)
+  })
+
+  try {
+    return await promise
+  } catch (err: any) {
+    handleRpcError(err, (unknownErr: any) => {
+      notification.notify({
+        text: "Unexpected error",
+        type: "error",
+      })
+
+      console.log(unknownErr);
+    })
+
+    throw err;
+  }
+}
+
+export const useAddImage = defineMutation(() => {
+  const imageBytes = ref<Uint8Array>(new Uint8Array)
+  const galleryId = ref("")
+  const description = ref("")
+  const extension = ref("")
+  const { mutate, ...mutation } = useMutation({
+    mutation: () => addImage(
+      imageBytes.value, 
+      galleryId.value, 
+      description.value, 
+      extension.value,
+    ),
+    onSettled: async () => {
+      const queryCache = useQueryCache()
+      await queryCache.invalidateQueries({ key: ['galleryImages'], exact: true })
+    }
+  })
+
+  return {
+    imageBytes,
+    galleryId,
+    description,
+    extension,
+    mutate,
+    mutation,
+  }
+})
