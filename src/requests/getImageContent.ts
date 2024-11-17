64f2b7be6eb67d50abec1a53d20a02ea03c9bef2
fatalistix <slavabal11@outlook.com>
2024-11-18 00:02:55 +0700

    method for loading image content

---
+++ src/requests/getImageContent.ts
@@ -0,0 +1,41 @@
+import { GetImageContentRequest, GetImageContentResponse } from "protos/gen/nai"
+import { useUserStore } from "../stores/UserStore"
+import { galleryServiceClient } from "../transport"
+import { handleRpcError } from "../utils/error_handler"
+import { useNotification } from "@kyvg/vue3-notification"
+
+const notification = useNotification()
+
+export const getImageContent = async (id: string): Promise<GetImageContentResponse> => {
+  const userStore = useUserStore()
+
+  const promise = userStore.withActualToken<GetImageContentRequest, GetImageContentResponse>(token => {
+    const opts = {
+      meta: {
+        "Authorization": "Bearer " + token,
+      }
+    }
+
+    const request = {
+      pImageUuid: id,
+    }
+
+    return galleryServiceClient.getImageContent(request, opts)
+  })
+
+  try {
+    return await promise
+  } catch (err: any) {
+    handleRpcError(err, (unknownErr: any) => {
+      notification.notify({
+        text: "Unexpected error",
+        type: "error",
+      })
+
+      console.log(unknownErr)
+    })
+
+    throw err
+  }
+}
+
