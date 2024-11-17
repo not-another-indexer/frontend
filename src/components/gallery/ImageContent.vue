a7c04c3d348a85b7ec03e820695121292b5de629
fatalistix <slavabal11@outlook.com>
2024-11-18 00:05:43 +0700

    added image wrapper

---
+++ src/components/gallery/ImageContent.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import Image from '../../assets/Image.vue';
+
+
+const props = defineProps<{
+  id: string,
+}>()
+
+const isDeleteHovered = ref(false)
+
+const deleteImageAction = () => {
+
+}
+
+</script>
+
+<template>
+  <div class="box">
+    <figure class="image has-radius-normal has-text-centered mb-3 has-background-text-90">
+      <Image :id="id" />
+    </figure>
+
+    <button class="button is-small is-fullwidth" :class="{'is-danger': isDeleteHovered}" v-on:mouseenter="isDeleteHovered = true" v-on:mouseleave="isDeleteHovered = false">
+      <span>
+        Delete
+      </span>
+      <span class="icon">
+        <i class="pi pi-trash" />
+      </span>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
