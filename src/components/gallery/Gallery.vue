f932beee1c986d99f2510a2d1df3813918267cf3
fatalistix <slavabal11@outlook.com>
2024-11-23 17:50:32 +0700

    added validation, parameters normalization, search displaying

--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -1,13 +1,19 @@
 <script setup lang="ts">
 import { useRoute, useRouter } from 'vue-router';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import AddImageModal from './AddImageModal.vue';
 import ImageContent from './ImageContent.vue';
 import { getGalleryImages } from '../../requests/getGalleryImages';
 import { useQuery } from '@pinia/colada';
 import { searchImages, SearchParameters } from '../../requests/searchImages';
 import { SearchImagesResponse } from 'protos/gen/nai';
+import { useNotification } from '@kyvg/vue3-notification';
+import { useQueryRules } from '../../validation/rules/search/query';
+import { useCountRules } from '../../validation/rules/search/count';
+import useVuelidate from '@vuelidate/core';
+import { helpers } from '@vuelidate/validators';
 
+const notification = useNotification()
 const router = useRouter()
 const route = useRoute()
 
@@ -35,26 +41,74 @@ const count = ref<bigint>(0n)
 
 const searchResult = ref<SearchImagesResponse | null>(null)
 
+const queryRules = useQueryRules()
+const countRules = useCountRules()
+
+const v = useVuelidate(
+  {
+    query: queryRules,
+    count: {
+      ...countRules,
+      maxValue: helpers.withMessage(
+        "The count must be less then total count",
+        (value: bigint) => data.value ? value <= data.value.pTotal : true,
+      ),
+    }
+  },
+  {
+    query,
+    count,
+  }
+)
+
+const queryErrorMessages = computed(() => v.value.query.$silentErrors.map(e => e.$message))
+const countErrorMessages = computed(() => v.value.count.$silentErrors.map(e => e.$message))
+
 const goToGalleries = () => {
   router.push("/user/galleries")
 }
 
 const searchAction = async () => {
+  const sum = semanticOnePeaceSimilarity.value + 
+      recognizedTextSimilarity.value + 
+      textualDescriptionSimilarity.value + 
+      recognizedFaceSimilarity.value + 
+      recognizedTextBm25Rank.value + 
+      textualDescriptionBm25Rank.value
+
+  
+
   const params: SearchParameters = {
-    SEMANTIC_ONE_PEACE_SIMILARITY: semanticOnePeaceSimilarity.value,
-    RECOGNIZED_TEXT_SIMILARITY: recognizedTextSimilarity.value,
-    TEXTUAL_DESCRIPTION_SIMILARITY: textualDescriptionSimilarity.value,
-    RECOGNIZED_FACE_SIMILARITY: recognizedFaceSimilarity.value,
-    RECOGNIZED_TEXT_BM25_RANK: recognizedTextBm25Rank.value,
-    TEXTUAL_DESCRIPTION_BM25_RANK: textualDescriptionBm25Rank.value,
+    SEMANTIC_ONE_PEACE_SIMILARITY: semanticOnePeaceSimilarity.value / sum,
+    RECOGNIZED_TEXT_SIMILARITY: recognizedTextSimilarity.value / sum,
+    TEXTUAL_DESCRIPTION_SIMILARITY: textualDescriptionSimilarity.value / sum,
+    RECOGNIZED_FACE_SIMILARITY: recognizedFaceSimilarity.value / sum,
+    RECOGNIZED_TEXT_BM25_RANK: recognizedTextBm25Rank.value / sum,
+    TEXTUAL_DESCRIPTION_BM25_RANK: textualDescriptionBm25Rank.value / sum,
   }
 
-  searchResult.value = await searchImages(
-    query.value,
-    id,
-    params,
-    count.value,  
-  )
+  try {
+    searchResult.value = await searchImages(
+      query.value,
+      id,
+      params,
+      count.value,  
+    )
+  } catch (err: any) {
+    if (err instanceof Error) {
+      notification.notify({
+        title: "Unable to show search result",
+        type: "error",
+        ignoreDuplicates: true,
+      })
+    } else {
+      notification.notify({
+        title: "Unable to show search result",
+        type: "error",
+        ignoreDuplicates: true,
+      })
+    }
+  } 
 }
 
 const goToPageAction = (page: number) => {
@@ -126,10 +180,17 @@ const incrementPageAction = () => {
  
         <div class="level-right">
           <nav class="level is-mobile">
-            <div class="level-item">
+            <div v-if="searchResult === null" class="level-item">
               <button class="button" :class="{ 'is-primary': isAddImageButtonHovered }" v-on:mouseenter="isAddImageButtonHovered = true" v-on:mouseleave="isAddImageButtonHovered = false" @click="isAddImageModalOpened = true">
                 Add image
               </button> 
+            </div>
+            <div v-else class="level-item">
+              <button class="button is-danger" @click="searchResult = null">
+                <span class="icon">
+                  <i class="pi pi-times" />
+                </span>
+              </button>
             </div>
           </nav>
         </div>
@@ -158,10 +219,13 @@ const incrementPageAction = () => {
     </div>
   </section>
   <section v-if="isSearchDropdownActive"class="hero is-small">
-    <div class="hero-body">
+    <div class="hero-body pt-0">
+      <button class="button is-fullwidth is-info mb-4" @click="searchAction" :disabled="v.query.$invalid || v.count.$invalid">
+        Find images
+      </button>
       <nav class="level">
         <div class="level-item">
-          <div class="container">
+          <div class="container is-fluid">
             <label class="label is-small my-0">Semantic one peace similarity</label>
             <input v-model="semanticOnePeaceSimilarity" class="slider is-small is-fullwidth is-primary is-circle has-output my-0" step="0.001" min="0" max="1" value="0.5" type="range" />
             <output class="has-text-primary-invert">{{ semanticOnePeaceSimilarity }}</output>
@@ -188,22 +252,48 @@ const incrementPageAction = () => {
           </div>
         </div>
         <div class="level-item">
-          <div class="container">
-            <label class="label is-small">Query</label>
-            <textarea v-model="query" class="textarea" placeholder="e.g. black cat"></textarea>
+          <div class="container is-fluid">
+            <div class="field">
+              <label class="label is-small">
+                Query
+              </label>
+              <textarea v-model="query" class="textarea" placeholder="e.g. black cat"></textarea>
+              <p v-if="v.query.$invalid" v-for="m in queryErrorMessages" class="help is-danger">{{ m }}</p>
+            </div>
+            <div class="field">
+              <label class="label is-small">
+                Count
+              </label>
+              <div class="control">
+                <input v-model="count" type="number" class="input" placeholder="e.g. 4">
+              </div>
+              <p v-if="v.count.$invalid" v-for="m in countErrorMessages" class="help is-danger">{{ m }}</p>
+            </div>
           </div>
         </div>
       </nav>
     </div>
   </section>
   
-  <div v-if="asyncStatus === 'idle' && data" class="container is-fluid">
-    <div v-if="data.pContent.length === 0" class="has-text-centered">
-      No available images.
+  <div v-if="searchResult === null">
+    <div v-if="asyncStatus === 'idle' && data" class="container is-fluid">
+      <div v-if="data.pContent.length === 0" class="has-text-centered">
+        No available images.
+      </div>
+      <div v-else class="grid">
+        <div class="cell" v-for="imageData in data.pContent">
+          <ImageContent :id="imageData.pImageId" :gallery-id="id" />
+        </div>
+      </div>
+    </div>
+  </div>
+  <div v-else class="container is-fluid">
+    <div v-if="searchResult.pContent.length === 0" class="has-text-centered">
+      No images found.
     </div>
     <div v-else class="grid">
-      <div class="cell" v-for="imageData in data.pContent">
-        <ImageContent :id="imageData.pImageId" :gallery-id="id" />
+      <div class="cell" v-for="imageData in searchResult.pContent">
+        <ImageContent :id="imageData.pImage!!.pImageId" :gallery-id="id" />
       </div>
     </div>
   </div>
