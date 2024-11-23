73a592c836fd0c1ea1e4eaaf7724233550af2111
fatalistix <slavabal11@outlook.com>
2024-11-23 17:49:08 +0700

    commented due to lack of usage

--- src/validation/rules/search/query.ts
+++ src/validation/rules/search/query.ts
@@ -1,5 +1,5 @@
 import { helpers, required } from "@vuelidate/validators";
 
 export const useQueryRules = () => ({
-  required: helpers.withMessage("The query must not be empty", required),
+  // required: helpers.withMessage("The query must not be empty", required),
 })
